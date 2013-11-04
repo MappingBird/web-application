b229ebdadbe579afb9532b167dbda0380abb4a7c
Mike Palmer <mhpalmer@gmail.com>
2013-11-04 21:39:10 +0800

    change INSTALLED_APPS order

--- src/django/pingismo/settings/base.py
+++ src/django/pingismo/settings/base.py
@@ -114,6 +114,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'base',
 
     # allauth
     'allauth',
@@ -123,7 +124,6 @@ INSTALLED_APPS = (
     'south',
     'rest_framework',
 
-    'base',
     'bucketlist',
 )
 
