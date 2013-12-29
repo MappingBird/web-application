3322ac54d237ae7b0f2aa9ea883b11d999542b64
Mike Palmer <mhpalmer@gmail.com>
2013-12-29 18:03:20 +0800

    Remove console calls

--- src/static/js/index-app/directives/directives.js
+++ src/static/js/index-app/directives/directives.js
@@ -5,8 +5,6 @@ directives.directive("buttonHref", [ '$compile', function($compile) {
     return {
         restrict: 'A',
         link: function(scope, element, attrs ) {
-            console.log('buttonHref');
-            console.log(attrs);
             element.on('click', function() {
                 window.location.href = attrs['href'];
             });
