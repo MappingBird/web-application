7f914f44870fb41db32eeb439ed51ebfc1940ad8
Mike Palmer <mhpalmer@gmail.com>
2014-07-09 00:16:30 +0800

    New save design, improved search results panel

--- src/static/js/save-app/directives/directives.js
+++ src/static/js/save-app/directives/directives.js
@@ -77,7 +77,7 @@ directives.directive('leftOuterHeight', function() {
         scope: false,
         controller: function($scope, $element, $attrs, BroadcastService) {
             // DIRTY HACK
-            var tolerance = 60; // pixels
+            var tolerance = 80; // pixels
             var height = $($('body')[0]).height() - $($('header')[0]).height() - $($('footer')[0]).height() - tolerance;
             $element.css('height', height + 'px');
         }
