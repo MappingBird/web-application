d4c15df0b2241faaefe1976e9f9fa3c33dbcd62d
Tom Chen <ctchen@gmail.com>
2014-08-02 23:17:22 +0800

    add stay parameter in homepage

--- src/django/base/views.py
+++ src/django/base/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import redirect
 from django.contrib.staticfiles.views import serve
 
 def home(request):
-    if request.user.is_authenticated():
+    stay = request.GET.get('stay')
+    if request.user.is_authenticated() and stay != '1' and stay != 1:
         return redirect('/static/app.html')
 
     return serve(request, path='/index.html')
