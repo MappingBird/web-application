2dcb864927cf26d175b4f1de077b7952f859f95b
Tom Chen <ctchen@gmail.com>
2014-08-09 14:14:35 +0800

    Cancel redirect in index

--- src/django/base/views.py
+++ src/django/base/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import redirect
 from django.contrib.staticfiles.views import serve
 
 def home(request):
+    '''
     stay = request.GET.get('stay')
     if request.user.is_authenticated() and stay != '1' and stay != 1:
         return redirect('/static/app.html')
+    '''
 
     return serve(request, path='/index.html')
