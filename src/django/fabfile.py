a36931e91fa691424f7c211021d5f8efb1b3179a
Tom Chen <ctchen@gmail.com>
2014-10-19 02:38:24 +0800

    Update fabfile

--- src/django/fabfile.py
+++ src/django/fabfile.py
@@ -5,18 +5,44 @@ from fabric.contrib.files import exists
 
 env.hosts = ['django@mappingbird.com']
  
-def get_code():
-    with cd('pingismo'):
-        run('git pull origin master')
+def get_code(stage=False):
+    directory = 'pingismo'
+    branch = 'master'
+
+    if stage:
+        directory = 'stage/pingismo'
+        branch = 'stage'
+
+    with cd(directory):
+        run('git pull origin %s' % branch)
+
+def copy_db():
+    with cd('pingismo/backup'):
+        run('./main2stage.sh')
  
-def collect_static():
-    with prefix('workon pingismo'), cd('pingismo/src/django'):
+def collect_static(stage=False):
+    directory = 'pingismo'
+
+    if stage:
+        directory = 'stage/pingismo'
+
+    with prefix('workon pingismo'), cd('%s/src/django' % directory):
         run('python manage.py collectstatic')
  
-def restart_pingismo():
-    sudo('service pingismo restart')
+def restart_pingismo(stage=False):
+    service = 'pingismo'
+
+    if stage:
+        service = 'mappingbird-stage'
+
+    sudo('service %s restart' % service)
 
 def deploy():
     get_code()
     collect_static()
     restart_pingismo()
+
+def stage():
+    get_code(stage=True)
+    collect_static(stage=True)
+    restart_pingismo(stage=True)
