e0d940865fbae408f3d8bfb40c05415c3e309ab2
eric0329 <asdguo@mail2000.com.tw>
2015-03-15 20:44:14 +0800

    fix working dir of Virtualenvwrapper

--- deploy/web.sh
+++ deploy/web.sh
@@ -4,7 +4,7 @@ DIR=$(dirname $0)
 BASEDIR=$DIR/../
 
 # virtualenv
-VENV=/home/django/.virtualenvs/pingismo
+VENV=/home/django/venvs/pingismo
 source $VENV/bin/activate
 source $VENV/bin/postactivate
 
@@ -14,7 +14,7 @@ GROUP=django
 NUM_WORKERS=3
 LOGFILE=/var/log/pingismo/gunicorn.log
 PID=/var/run/gunicorn.pid
-PORT=9001
+PORT=9002
 BIND_IP=127.0.0.1:$PORT
 LANG=en_US.UTF-8
 
