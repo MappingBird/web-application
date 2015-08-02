eb1b24fb021c6ada7400421ff5f3b67d71cf1815
eric0329 <asdguo@mail2000.com.tw>
2015-08-02 11:31:48 +0800

    update barnowl.py

--- prototypes/owl/owl/barnowl.py
+++ prototypes/owl/owl/barnowl.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 import solr
 
 #-- Solr client
 class BarnOwl:
     s_ = None
-    sUrl_ = "http://127.0.0.1:8983/solr/posts"
+    sUrl_ = "http://45.33.63.238:8983/solr/posts"
     #TODO... logging
 
     def __init__(self, serverUrl = None):
@@ -15,6 +16,45 @@ class BarnOwl:
     def getPointByUrl(self, qUrl):
         qUrl = "\"{0}\"".format(qUrl)
         q = "{0}:{1}".format("url", qUrl)
+        print q
+
+        resp = {}
+        try:
+            resp = self.s_.query(q)
+        except Exception, e:
+            print e
+        
+        for p in resp:
+            if "id" in p: p.pop("id")
+            if "url2point" in p: p.pop("url2point")
+            if "content" in p: p.pop("content")
+            if "review" in p: p.pop("review")
+
+        return resp
+
+    def getPointByUrl(self, qUrl):
+        qUrl = "\"{0}\"".format(qUrl)
+        q = "{0}:{1}".format("url", qUrl)
+#        print q
+
+        resp = {}
+        try:
+            resp = self.s_.query(q)
+        except Exception, e:
+            print e
+        
+        for p in resp:
+            if "id" in p: p.pop("id")
+            if "url2point" in p: p.pop("url2point")
+            if "content" in p: p.pop("content")
+            if "review" in p: p.pop("review")
+
+        return resp
+
+    def getPointByKeyword(self, kw):
+        qAN = u"\"{0}\"".format(kw)
+        qPN = u"\"{0}\"".format(kw)
+        q = u"{0}:{1} {2}:{3}".format("article_name", qAN, "place_name", qPN)
         print q
 
         resp = {}
@@ -64,4 +104,12 @@ if __name__ == "__main__":
         #print p["article_name"]
         print p["latlng"].split(',')        
         #print p["address"]
+
+    resp = b.getPointByKeyword(u"宜蘭 玻璃屋")
+    print len(resp)
+    for p in resp:
+        print p["article_name"]
+        print p["latlng"].split(',')        
+        print p["address"]
+
 
